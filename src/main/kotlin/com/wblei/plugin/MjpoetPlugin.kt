74c7f39f452aae4e7b5917ae9abe73b59bf0b7eb
leiweibo <leiweibo@gmail.com>
2021-04-21 19:16:02 +0800

    init commit of the basic skeleton of the plugin

--- src/main/kotlin/com/wblei/plugin/MjpoetPlugin.kt
+++ src/main/kotlin/com/wblei/plugin/MjpoetPlugin.kt
@@ -4,9 +4,13 @@
 package com.wblei.plugin
 
 import com.android.build.gradle.AppExtension
-import groovy.lang.Closure
+import com.android.build.gradle.api.ApplicationVariant
+import groovy.util.Node
+import groovy.util.XmlParser
+
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import java.io.File
 import java.lang.IllegalArgumentException
 
 /**
@@ -24,12 +28,40 @@ class MjpoetPlugin: Plugin<Project> {
                 val mjpoet = project.extensions.getByName("mjpoet") as MjpoetConfig
                 if (it.name in mjpoet.variants) {
     
-                    print("hello wrold......\n")
-//                print((mjpoet.mjconfig?: error("no config for release")).call().packageBase)
-                    println(mjpoet.packageBase)
-                    println(it.name)
+//                    print("hello wrold......\n")
+//                    println(mjpoet.packageBase)
+//                    println(it.name)
+//                    println((it as ApplicationVariant).sourceSets)
+                    
+                    var dir = File(project.buildDir, "generated/source/mj/${it.name}")
+                    var javaDir = File(dir, "java")
+                    var resDir = File(dir, "res")
+                    var manifestFile = File(dir, "AndroidManifest.xml")
+                    
+                    val mjTask = project.tasks.register("generate${it.name}MJCode", MjpoetTask::class.java) { task ->
+                        task.config = mjpoetConfig
+                        task.outDir = dir
+                        task.appPackageName = getPackageName((it as ApplicationVariant))
+                    }
+                    it.registerJavaGeneratingTask(mjTask.get(), javaDir)
                 }
             }
         }
+    }
+    
+    fun getPackageName(variant: ApplicationVariant):String? {
+        val sourceSets = variant.sourceSets
+        var packageName : String? = null
+        for (sourceSet in sourceSets) {
+            if (sourceSet.manifestFile.exists()) {
+                val xmlParser = XmlParser()
+                val node: Node = xmlParser.parse(sourceSet.manifestFile)
+                packageName = node.attribute("package") as String
+                if (packageName.isNotEmpty()) {
+                    break
+                }
+            }
+        }
+        return packageName
     }
 }
--- src/main/kotlin/com/wblei/plugin/MjpoetTask.kt
+++ src/main/kotlin/com/wblei/plugin/MjpoetTask.kt
@@ -1,13 +1,80 @@
 package com.wblei.plugin
 
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.TypeSpec
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Nested
+import org.gradle.api.tasks.OutputDirectories
 import org.gradle.api.tasks.TaskAction
+import java.io.File
+import javax.lang.model.element.Modifier
 
-class MjpoetTask : DefaultTask() {
-  @Nested val config: MjpoetConfig = MjpoetConfig()
+open class MjpoetTask : DefaultTask() {
+  @Nested
+  var config: MjpoetConfig = MjpoetConfig()
   
-  @TaskAction fun start() {
+  @Input
+  var appPackageName: String? = ""
+  
+  @OutputDirectories
+  lateinit var outDir: File
+  
+  @TaskAction
+  fun start() {
     print("the package base name is: ${config.packageBase}")
+    
+    if (outDir.exists()) {
+      outDir.delete()
+    }
+    generateJavaClasses()
+  }
+  
+  /**
+   * generate the classes.
+   */
+  private fun generateJavaClasses() {
+    val className = "TestActivity"
+    val layoutName = "activity_test"
+    
+    generateSingleClass(className, layoutName)
+  }
+  
+  /**
+   * generate the single class base on the class name and layout name parameters.
+   * @param className: class name
+   * @param layoutName: layout name
+   */
+  private fun generateSingleClass(className: String, layoutName: String) {
+    var javaDir = File(outDir, "java")
+    val packageName = "${config.packageBase}"
+    val typeBuilder = TypeSpec.classBuilder(className)
+    typeBuilder.superclass(ClassName.get("android.app", "Activity"))
+    typeBuilder.addModifiers(Modifier.PUBLIC)
+    val bundleClassName = ClassName.get("android.os", "Bundle")
+    val rClassName = ClassName.get(appPackageName, "R")
+    typeBuilder.addMethod(MethodSpec.methodBuilder("onCreate")
+     .addModifiers(Modifier.PROTECTED)
+     .addParameter(bundleClassName, "savedInstanceState")
+     .addStatement("super.onCreate(savedInstanceState)")
+     .addStatement("setContentView(${rClassName}.layout.$layoutName)")
+     .build())
+    
+    val fileBuilder = JavaFile.builder(packageName, typeBuilder.build())
+    fileBuilder.build().writeTo(javaDir)
+  }
+  
+  private fun generateLayout(layoutName: String) {
+    val resFile = File(outDir, "res/layout/${layoutName}.xml")
+    if (!resFile.parentFile.exists()) {
+      resFile.parentFile.mkdirs()
+    }
+    if (!resFile.exists()) {
+      resFile.createNewFile()
+    }
+    
+    
   }
 }
